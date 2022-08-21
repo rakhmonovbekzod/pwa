8801f73d7d92be6e974c8e0a93009319893b0334
bekzoddeveloper1995 <bekzodrakhmonov1995@gmail.com>
2022-08-21 10:44:17 +0500

    chack  fallback

--- next.config.js
+++ next.config.js
@@ -1,15 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
 
-module.exports = nextConfig
 const withPWA = require('next-pwa');
 
 module.exports = withPWA({
   pwa: {
     dest: 'public',
+    swSrc: 'service-worker.js',
   },
-  reactStrictMode: true,
-})
+});
--- pages/payment.js
+++ pages/payment.js
@@ -1,15 +1,31 @@
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import PaymentForm from "../components/PaymentForm";
 
 const Payment = () => {
     const router = useRouter()
+    const [user,setUser] = useState([])
     const { query } = router;
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json()).then(d => setUser(d))
+        
+    },[])
 
     return <>
         <div className="container">
             <div className="mt-40">
                 <PaymentForm product={query} />
             </div>
+            {
+                user.length && user.map((item,index) => {
+                    return <>
+                       <div>
+                          <h2>{item.name}</h2>
+                          <p>{item.email}</p>
+                       </div>
+                    </>
+                })
+            }
         </div>
     </>
 }
