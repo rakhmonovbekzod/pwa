d3b27c0ebdbe619857942efc3979a10614765980
bekzoddeveloper1995 <bekzodrakhmonov1995@gmail.com>
2022-08-21 19:23:12 +0500

    fixed cors for error status

--- pages/api/pay.js
+++ pages/api/pay.js
@@ -7,13 +7,18 @@ export default await function handler(req, res) {
     await NextCors(req, res, {
         methods: ['POST'],
         origin: '*',
-        optionsSuccessStatus: 200,
+        optionsErrorStatus: 401,
     })
     if (!checkAuth(req.headers.authorization)){
         return res.status(401).json({
             success: false, error: "Unauthorized"
         })
     }
+    await NextCors(req, res, {
+        methods: ['POST'],
+        origin: '*',
+        optionsErrorStatus: 400,
+    })
     const data = req.body
     if (!checkData(data)){
         
@@ -22,11 +27,21 @@ export default await function handler(req, res) {
             success: false, error: "Bad Request"
         })
     }
+    await NextCors(req, res, {
+        methods: ['POST'],
+        origin: '*',
+        optionsErrorStatus: 400,
+    })
     if (!checkMin(data.payment, data.amount)) {
         return res.status(400).json({
             success: false, error: "Bad Amount"
         })
     }
+    await NextCors(req, res, {
+        methods: ['POST'],
+        origin: '*',
+        optionsSuccessStatus: 400,
+    })
     res.status(200).json({ 
         success: true, error: ""
     })
